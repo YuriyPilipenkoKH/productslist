11877d7070b35e390fc0ad084b716f28d5ce6e90
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-06 22:11:13 +0300

    access to categories

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
+import { currentUser } from '@clerk/nextjs/server'
 import React from 'react'
+import prisma from '@/lib/prisma'
 
 async function Dashboard() {
+  const user = await currentUser()
+  const creator = user?.firstName
+  const categories = await prisma.category.findMany({
+    where: {
+      creator
+    }
+  })
   return (
     <section className='p-24 space-y-6 min-h-screen flex flex-col'>
         Dashboardpage
