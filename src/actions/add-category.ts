cb3a72a17fa98451c8e3c60397ef5e4b2f7855e8
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-07 06:48:49 +0300

    addCategory sarver action templ

--- src/actions/add-category.ts
+++ src/actions/add-category.ts
@@ -1,2 +1,10 @@
 'use server'
 
+import prisma from "@/lib/prisma"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
+export const addCategory = async (formData: FormData) => {
+    const {name, creator} = Object.fromEntries(formData)
+    console.log(name, creator)
+}
