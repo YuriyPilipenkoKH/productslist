f1e7aab15fe122be4f01465fad68deadfc25e6d8
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-07 08:30:13 +0300

    completed update action

--- src/actions/update-category.ts
+++ src/actions/update-category.ts
@@ -3,3 +3,20 @@
 import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
+
+
+export const updateCategory = async (formData: FormData) => {
+    const {name, id} = Object.fromEntries(formData)
+    console.log(name)
+    try {
+        const updatedCategory = await prisma.category.update({
+            where: { id },
+            data: { name }
+        })
+    }
+     catch (error) {
+        console.log('Error'+ error)
+    }
+    revalidatePath('/dashboard')
+    redirect('/dashboard')
+}
