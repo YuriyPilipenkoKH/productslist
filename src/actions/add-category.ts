3f2551989a823aa3a29f069a93e5072a25a03b64
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-07 06:54:27 +0300

    new category creation

--- src/actions/add-category.ts
+++ src/actions/add-category.ts
@@ -7,4 +7,15 @@ import { redirect } from "next/navigation"
 export const addCategory = async (formData: FormData) => {
     const {name, creator} = Object.fromEntries(formData)
     console.log(name, creator)
+    try {
+        const newCategory = await prisma.category.create({
+            data: {
+                name,
+                creator
+            }
+        })
+    }
+     catch (error) {
+        console.log('Error'+ error)
+    }
 }
