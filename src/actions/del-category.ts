fa906e7dd8626b1ad6b75b45a90b8dff79d27ae5
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-07 07:37:24 +0300

    completed delete action

--- src/actions/del-category.ts
+++ src/actions/del-category.ts
@@ -3,3 +3,18 @@
 import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
+
+export const dsleteCategory = async (formData: FormData) => {
+    const id = formData.get('id')
+    console.log(id)
+    try {
+        await prisma.category.delete({
+            where: { id }
+        })
+    }
+     catch (error) {
+        console.log('Error'+ error)
+    }
+    revalidatePath('/dashboard')
+    // redirect('/')
+}
