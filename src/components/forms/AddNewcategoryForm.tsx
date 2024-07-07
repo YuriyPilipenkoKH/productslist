60b657afaa926923d8e0f22d9ffd7984e939360d
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-07 07:26:51 +0300

    my first server action!

--- src/components/forms/AddNewcategoryForm.tsx
+++ src/components/forms/AddNewcategoryForm.tsx
@@ -1,4 +1,5 @@
 import { addCategory } from '@/actions/add-category'
+import { Button, Input } from '@nextui-org/react'
 import React from 'react'
 
 interface AddNewCategoryFormProps {
@@ -9,13 +10,20 @@ const AddNewCategoryForm: React.FC<AddNewCategoryFormProps> = ({
 	creator
 	}) => {
   return (
-    <form action={addCategory}>
+    <form 
+		action={addCategory}
+		className='flex flex-col gap-2'>
 			<input
 				hidden
 				id='creator'
 				name='creator'
 				defaultValue={creator}
 				/>
+			<Input 
+			name='name'
+			id='name'
+			placeholder='category name'/>
+			<Button type='submit'>Add New category</Button>
     </form>
   )
 }
