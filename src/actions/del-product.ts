f5d9b7ee365cf1d8dfcf9b529cb61843ee472932
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-08 08:02:23 +0300

    complete action

--- src/actions/del-product.ts
+++ src/actions/del-product.ts
@@ -2,3 +2,20 @@
 
 import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
+
+export const deleteProduct = async (formData: FormData) => {
+    const id = formData.get('id') as string;
+    if (typeof id !== 'string') {
+        throw new Error('Form values must be strings');
+      }
+    console.log(id)
+    try {
+        await prisma.product.delete({
+            where: { id }
+        })
+    }
+     catch (error) {
+        console.log('Error'+ error)
+    }
+    revalidatePath('/dashboard')
+}
