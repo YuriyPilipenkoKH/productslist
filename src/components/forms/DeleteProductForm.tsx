d802dc57706a1e2a56409c3596cde6d8a87ee8fe
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-08 08:06:27 +0300

    del form complete

--- src/components/forms/DeleteProductForm.tsx
+++ src/components/forms/DeleteProductForm.tsx
@@ -1,3 +1,5 @@
+import { deleteProduct } from '@/actions/del-product';
+import { Button } from '@nextui-org/react';
 import React from 'react'
 
 interface DeleteProductFormProps {
@@ -6,7 +8,17 @@ interface DeleteProductFormProps {
 
 const DeleteProductForm: React.FC<DeleteProductFormProps> = ({ id }) => {
   return (
-    <div>DeleteProductForm</div>
+    <form action={deleteProduct}>
+    <input
+      hidden
+      name='id'
+      defaultValue={id}
+    />
+    <Button
+     type='submit'
+     variant='light'>
+      Delete Category</Button>
+  </form>
   )
 }
 
