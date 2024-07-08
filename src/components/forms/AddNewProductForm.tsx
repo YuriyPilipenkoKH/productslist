cb0458f5da5257486a67f026c7222fce36810950
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-08 07:42:43 +0300

    form template

---
+++ src/components/forms/AddNewProductForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+interface ddNewProductFormProps {
+	creator: string
+}
+
+
+const AddNewProductForm: React.FC<ddNewProductFormProps> = ({
+	creator
+	}) => {
+  return (
+    <div>AddNewProductForm</div>
+  )
+}
+
+export default AddNewProductForm
