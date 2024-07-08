f216e3b53741e31decf96188138914e8f63664b1
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-08 07:25:31 +0300

    complete addprod srver action

--- src/actions/add-product.ts
+++ src/actions/add-product.ts
@@ -3,3 +3,27 @@
 import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
+
+export const addProduct = async (formData: FormData) => {
+    const name = formData.get('name') as string;
+    const categoryId = formData.get('categoryId') as string;
+  
+    if (typeof name !== 'string' || typeof categoryId !== 'string') {
+      throw new Error('Form values must be strings');
+    }
+    console.log(name, categoryId)
+    try {
+        const newProduct = await prisma.product.create({
+            data: {
+                name,
+                categoryId
+            }
+        })
+        console.log('Category created:', newProduct);
+    }
+     catch (error) {
+        console.log('Error'+ error)
+    }
+    revalidatePath('/dashboard')
+    redirect('/')
+}
