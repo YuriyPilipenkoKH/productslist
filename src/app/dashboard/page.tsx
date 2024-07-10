817441df4f03eae2f134020e7422fbb4804ce7ad
Yuriy Pilipenko <yurik2061@gmail.com>
2024-07-10 20:20:38 +0300

    fin table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,15 +40,15 @@ async function Dashboard() {
             </thead>
             <tbody>
               {categories.map((category:Category, idx:number) => (
-                <tr key={idx}>
-                  <td className='px-2 py-3 text-sm font-normal text-left text-gray-900 border-b border-gray-200'>
+                <tr key={idx} >
+                  <td className='h-[57px] px-2 py-3 text-sm font-normal text-left text-gray-900 border-b border-gray-200'>
                     <Link 
                     className='mtext'
                     href={`/dashboard/category/${category.id}`}>
                       {capitalize(category.name)}
                     </Link>
                   </td>
-                  <td className= ' flex items-center gap-4 px-1 py-3 text-sm font-normal text-center border-b border-gray-200'>
+                  <td className= 'h-[57px] flex items-center gap-4 px-1 py-3 text-sm font-normal text-center border-b border-gray-200'>
                     <ShowDetails
                     className='mtext'
                     href={`/dashboard/category/${category.id}/update`}>
