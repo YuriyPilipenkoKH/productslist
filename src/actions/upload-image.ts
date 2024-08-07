4814b3ce68856fd320b4e56a86430b5bc5d4b1bf
Yuriy Pilipenko <yurik2061@gmail.com>
2024-08-07 22:26:53 +0300

    server action completed

--- src/actions/upload-image.ts
+++ src/actions/upload-image.ts
@@ -2,22 +2,62 @@
 
 import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
+import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 export const uploadImage = async (formData: FormData) => {
-    const image = formData.get('image') as string;
+    const image = formData.get('image') as File | null;
     const id = formData.get('id') as string;
   
-    if (typeof image !== 'string' || typeof id !== 'string') {
-      throw new Error('Form values must be strings');
+    if (!image || typeof id !== 'string') {
+      throw new Error('Invalid form values');
     }
- 
     
     try {
-        const updatedProduct = await prisma.product.update({
-            where: { id },
-            data: {  }
-        })
+        const bytes = await image?.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+    
+    
+            // Specify a constant filename based on userId
+        const filename = `${id}`; // Or use appropriate file extension
+    
+        // Upload file buffer to Cloudinary using upload_stream
+        const result: UploadApiResponse = await new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          { 
+            resource_type: "image",
+            folder: `productslist/products/${id}`,
+            public_id: filename, // Use the constant filename
+            overwrite: true, // Specify overwrite to replace existing files
+    
+          },
+          (error?: Error, result?: UploadApiResponse) => {
+            if (error) {
+              reject(error);
+            } 
+            else if (result) {
+              resolve(result);
+            } 
+            else {
+              reject(new Error("Unknown error occurred during upload"));
+            }
+          }
+        );
+        stream.end(buffer);
+      });
+      console.log(`curl`, result?.secure_url)
+
+
+      const updatedProduct = await prisma.product.update({
+        where: { id },
+        data: { imageUrl: result.secure_url }, // Assuming you have an imageUrl field in your product schema
+      });
         revalidatePath(`/dashboard`)
         return { success: true, updatedProduct  };
     }
