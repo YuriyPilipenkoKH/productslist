7bbac477512624b2b594019f495c85d8840563f5
Yuriy Pilipenko <yurik2061@gmail.com>
2024-08-13 21:26:42 +0300

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
       },
+      basePath: '/productslist', // Replace with your desired base path
 };
 
 export default nextConfig;
