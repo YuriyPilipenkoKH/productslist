d19dad28be1537174d96c38f1587c3b895f45955
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-01 12:09:39 +0200

    retrieveUserId() conceiled

--- src/lib/retrieveUserId.ts
+++ src/lib/retrieveUserId.ts
@@ -1,15 +1,14 @@
 "use server"
-
-import { currentUser } from "@clerk/nextjs/server";
-
+ 
 
 export async function retrieveUserId() {
-    const user = await currentUser()
-    if(!user) {
-      console.log('User not found')  
-      }
-    if(user) {
-        console.log('UserID', user?.id);
-       return user?.id 
-      }
+    // const user = await currentUser()
+
+    // if(!user) {
+    //   console.log('User not found')  
+    //   }
+    // if(user) {
+    //     console.log('UserID', user?.id);
+    //    return user?.id 
+    //   }
 }
