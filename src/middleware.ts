d5d50ad661cafcb70ec65d88a6073c371b0e7c55
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-04 12:15:28 +0200

    added matcher from clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,3 +2,11 @@ import authConfig from "../auth.config";
 import NextAuth from "next-auth";
 
 const {auth} = NextAuth(authConfig)
+export const config = {
+  matcher: [
+
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)"
+  ]
+};
