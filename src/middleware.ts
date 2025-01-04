abe0238473835320addb04a2bfdaa7ce90acd025
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-04 12:19:20 +0200

    logging path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
+
 import authConfig from "../auth.config";
 import NextAuth from "next-auth";
 
 const {auth} = NextAuth(authConfig)
+
+export default auth(async (req) => {
+// custom middleware logic
+console.log('Middleware called' , req.nextUrl.pathname);
+
+})
 export const config = {
   matcher: [
 
