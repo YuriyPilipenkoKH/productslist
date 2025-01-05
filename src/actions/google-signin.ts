fab8834b3233cb6e713fac472b5c8208819aa7c1
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-05 06:19:00 +0200

    wrapped signin in try-catch

--- src/actions/google-signin.ts
+++ src/actions/google-signin.ts
@@ -1,6 +1,20 @@
 'use server'
+import { revalidatePath } from "next/cache";
 import { signIn } from "../../auth";
 
 export const googleSignIn = async () => {
- return await signIn("google");
+  try {
+     await signIn("google");
+      revalidatePath('/dashboard')
+      return { success: true, };
+      
+  } catch (error) {
+    console.log('Error'+ error)
+    let errorMessage = 'An unexpected error occurred';
+    if (error instanceof Error) {
+        errorMessage = error.message;
+    }
+    revalidatePath('/login')
+    return { success: false, error: errorMessage };
+  }
 }
