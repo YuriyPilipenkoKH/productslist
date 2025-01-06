9de85de6204b39c99d318b9fb39d86a907277e35
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-06 12:42:01 +0200

    applied client side mounted to Login btn

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,20 +4,24 @@ import ThemeChanger from './ThemeChanger';
 import { LogInButton, LogoutButton } from './Button/Button';
 import { useSession } from "next-auth/react";
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 
 function NavBar() {
   const currentPath = usePathname();
   const { data: session, status } = useSession();
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() =>  setMounted(true), [])
   // const {user} = session
 
   return (
     <nav className='flex items-center justify-between p-4 border-b-slate-200'>
         <Logo/>
         <ThemeChanger />
-        {session 
+        {mounted && session 
         ?  <LogoutButton/> 
-        : (currentPath !== '/login') &&  <LogInButton/>}   
+        : (currentPath !== '/login') && mounted  &&  <LogInButton/>}   
     </nav>
   )
 }
