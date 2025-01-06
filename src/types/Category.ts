e8d674991423a932a5a6298e2a5fceb2e1bea873
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-06 14:54:49 +0200

    rm extra product types

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -1,17 +1,6 @@
 import { Category, Product } from '@prisma/client';
 
 
-// export interface Category {
-//   id: string;
-//   name: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   products: Product[];
-//   creator: string;
-//   userId : string;
-
-// }
-
 export type CategoryWithProducts = Category & {
   products: Product[];
 };
