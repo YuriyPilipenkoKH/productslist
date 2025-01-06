7d405f1f4f26ccb54db1ac4438ccef95caf6c0cb
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-06 10:54:55 +0200

    made logo link dynamic

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
@@ -6,9 +7,10 @@ import React, { useState } from 'react'
 function Logo() {
   const [isOptimized, setIsOptimized] = useState(false)
   const [isloading, setIsloading] = useState(false)
-  
+   const { data: session } = useSession();
+
   return (
-    <Link href='/'>
+    <Link href={session ? '/dashboard' : '/'}>
         <Image 
           src='https://res.cloudinary.com/dwdkw1a4j/image/upload/v1720469699/productslist/logo/racvswvmivsj7kx8z2ta.png' 
           alt='icon' 
