19c1b4c2a8226ad95098b1c82b228819a2edae1f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-18 08:15:59 +0200

    replaced domains with remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Google OAuth avatars
       },
-      // basePath: '/productslist', // Replace with your desired base path
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com', // GitHub avatars
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // Cloudinary images
+      },
+    ],
+  },
+  // basePath: '/productslist', // Replace with your desired base path
 };
 
 export default nextConfig;
+
+// images: {
+//     domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
+//   },
